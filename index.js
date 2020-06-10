46364dd0d41bdda97cc4e0222359e3511266a027
Kimberlee Johnson <kimee.johnson@gmail.com>
2020-06-10 11:24:33 -0700

    Added index.html with barebones callFrame

---
+++ index.js
@@ -0,0 +1,19 @@
+/**
+ * Handles request and response routing 
+ */
+const handler = require("serve-handler");
+
+module.exports = async (req, res) => {
+  console.log(req.url);
+  await handler(req, res, {
+    public: "../",
+    cleanUrls: false,
+    unlisted: [
+      "index.js",
+      "package.json",
+      "package-lock.json",
+      "doc-site-assets",
+      "node_modules"
+    ]
+  });
+};
